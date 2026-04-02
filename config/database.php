44cd2f647223e932c4a43f92f0ccc008aed4086d
alideeb-coder <alideeb566056@gmail.com>
2026-04-02 10:28:25 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,14 +5,24 @@ class Database{
     private $username="3qXuqz7eF29TbWY.root";
     private $password="";
     private $conn=null;
+    public function __construct() {
+        // استخدام getenv() لقراءة القيم التي وضعناها في Render
+        $this->host     = getenv('DB_HOST');
+        $this->db_name  = getenv('DB_NAME');
+        $this->username = getenv('DB_USER');
+        $this->password = getenv('DB_PASS');
+    }
     public function getConnection(){
         try{
-            $this->conn=new PDO(
-            "mysql:host={$this->host};dbname={$this->db_name};",
-            $this->username,
-            $this->password,
-            [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION ]
-        );
+            $this->conn = new PDO(
+                "mysql:host={$this->host};port=4000;dbname={$this->db_name};",
+                $this->username,
+                $this->password,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_SSL_CA => true 
+                ]
+            );
         $this->conn->exec("set names utf8");
         } 
         catch(PDOException $exception){
